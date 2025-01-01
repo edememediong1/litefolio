81f778ec4a350ead07b48634801cee1ff0b117a2
Emediong Bassey Edem <edememediong1@gmail.com>
2025-01-01 15:16:20 +0100

    fixed the accordion using grid

--- src/sections/Experiences.jsx
+++ src/sections/Experiences.jsx
@@ -44,27 +44,28 @@ function Experiences() {
 
     <div className="h-auto bg-primary-bg p-10">
         <p className="text-[55px] font-jamjuree font-bold bg-gradient-to-r from-lemon via-purple to-dark-light bg-clip-text text-transparent">Work Experiences</p>
-        <p className="text-[20px] font-jamjuree text-dark-light mt-[20px]">With 2 years of experience as a designer and developer, I have worked with many clients worldwide, built numerous frontend, backend, and AI applications, managed various projects, and contributed to Opensource projects</p>
+        <p className="text-[20px] font-jamjuree text-dark-light mt-[20px] mb-[25px]">With 2 years of experience as a designer and developer, I have worked with many clients worldwide, built numerous frontend, backend, and AI applications, managed various projects, and contributed to Opensource projects</p>
         <div className="grid grid-cols-2 gap-10">
             {data.map((item, index) => {
 
               return (
                 <div key={index} className={` bg-bg-light transition-all duration-300 ease-in-out ${activeIndex === index ? 'h-auto': 'h-[133px] overflow-hidden'}`}>
-                    <div  onClick={() => toggleAccordion(index)} className="flex justify-between items-center cursor-pointer h-[133px]">
-                      <h2 className="flex justify-center items-center font-jamjuree text-[30px] h-[133px] w-[17%] text-dark-light font-bold">{item.year}</h2>
-                      <section>
-                        <h3 className="text-xl ">{item.role}</h3>
-                        <p>{item.company}</p>
-                        <p>{item.location}</p>
+                    <div  onClick={() => toggleAccordion(index)} className="grid grid-cols-5 cursor-pointer h-[133px] ">
+                      <h2 className="col-span-1 flex justify-center items-center font-jamjuree text-[30px] h-[133px] font-bold text-[#c2bad2] bg-[#252f45]">{item.year}</h2>
+                      <section className="col-span-3 flex-col justify-center items-start bg-[#1b2335] p-4">
+                        <h3 className="text-xl text-[25px] font-bakbak bg-gradient-to-r from-lemon via-purple to-dark-light bg-clip-text text-transparent">{item.role}</h3>
+                        <p className="text-[20px] text-[#c2bad2] font-jamjuree">{item.company}</p>
+                        <p className="text-[20px] text-[#c2bad2] font-jamjuree">{item.location}</p>
                       </section>
-                      <span className="text-xl">
+                      <span className="col-span-1  flex justify-center items-center text-xl">
                           {activeIndex === index ? "-": "+"}
                       </span>
                     </div>
                     {activeIndex === index ? (
                       <div className="left-0 right-0 z-10 p-4 bg-bg-light h-[300px] ">
-                        <p>{item.description}</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui voluptas, autem numquam, reprehenderit voluptatem hic corrupti dolore iste cumque architecto libero provident, ipsam pariatur. Quos temporibus necessitatibus, ipsa adipisci quam suscipit nisi nemo nulla error, ratione deserunt ea, maiores facilis iure. Eaque, cum culpa. Quisquam temporibus eligendi culpa adipisci eius!</p>
+                        <p className="text-[18px] text-[#c2bad2] font-jamjuree mb-[10px]">{item.description}</p>
+                        <p className="text-[18px] text-[#c2bad2] font-jamjuree mb-[10px]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui voluptas, autem numquam, reprehenderit voluptatem hic corrupti dolore iste cumque architecto libero provident, ipsam pariatur</p>
+                        <p className="text-[18px] text-[#c2bad2] font-jamjuree mb-[10px]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui voluptas, autem numquam, reprehenderit voluptatem hic corrupti dolore iste cumque architecto libero provident, ipsam pariatur</p>
                       </div> 
                     ) : null}
                 </div>
