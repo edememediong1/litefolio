6482932cb67581ac65abdf43553be4ac5a7d7f4c
Emediong Bassey Edem <edememediong1@gmail.com>
2025-01-05 23:50:21 +0100

    added the last feature for the about

--- src/sections/Tools.jsx
+++ src/sections/Tools.jsx
@@ -10,7 +10,7 @@ import css from "../assets/css.svg"
 function Tools() {
   return (
     <div className="tools-cont h-screen  bg-primary-bg p-10">
-        <h2 className="tools-head text-[3.5rem] font-[700] font-jamjuree bg-gradient-to-r from-lemon via-dark-light to-purple bg-clip-text text-transparent">Tools I have used</h2>
+        <p className="tools-head text-[3.5rem] font-[700] font-jamjuree bg-gradient-to-r from-lemon via-dark-light to-purple bg-clip-text text-transparent">Tools I have used</p>
         <p className="tools-text text-[1.25rem] font-jamjuree text-dark-light mt-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita alias possimus odio odit tempora assumenda quibusdam laboriosam sint sunt obcaecati doloribus, quo neque nulla quam quos fuga doloremque enim facere.</p>
         <article className="tools-item flex justify-between items-center h-[70%]">
             <section className="tool-one w-[50%] flex justify-start items-center">
